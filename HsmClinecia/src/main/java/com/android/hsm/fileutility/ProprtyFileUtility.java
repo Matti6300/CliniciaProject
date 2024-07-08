ed4345277fdb1d461ed01804ae9d7fad81559f5b
bharath8858 <105233330+bharath8858@users.noreply.github.com>
2024-07-08 11:25:00 +0530

    hi

--- HsmClinecia/src/main/java/com/android/hsm/fileutility/ProprtyFileUtility.java
+++
@@ -1,5 +0,0 @@
-package com.android.hsm.fileutility;
-
-public class ProprtyFileUtility {
-
-}
--- HsmClinecia/src/main/java/com/android/hsm/generalutility/BaseClass.java
+++ HsmClinecia/src/main/java/com/android/hsm/generalutility/BaseClass.java
@@ -1,5 +1,56 @@
 package com.android.hsm.generalutility;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.time.Duration;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import com.android.hsm.fileutility.PropertyFileUtility;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+
 public class BaseClass {
+	
+	public AndroidDriver driver;
+	AppiumDriverLocalService service;
+	
+	PropertyFileUtility pu= new PropertyFileUtility();
+	
+	@BeforeSuite
+	public void ConectingToServer() throws IOException {
+		DesiredCapabilities dc= new DesiredCapabilities();
+		dc.setCapability("platformName", "android");
+		dc.setCapability("automationName", "uiautomator2");
+		dc.setCapability("deviceName", pu.getTheDataFromPropertyfile("devicename"));
+		dc.setCapability("UDID", pu.getTheDataFromPropertyfile("udid"));
+		dc.setCapability("noReset", true);
+		dc.setCapability("ignoreHiddenApiPolicyError", true);
+		dc.setCapability("autoGrantPermission", true);
+		URL u =new URL(pu.getTheDataFromPropertyfile("url"));
+		driver =new AndroidDriver(u,dc);
+		 service = new AppiumServiceBuilder().withAppiumJS(new File(pu.getTheDataFromPropertyfile("jsPath"))).
+				 withIPAddress(pu.getTheDataFromPropertyfile("ip")).usingPort(4723)
+				 .withTimeout(Duration.ofSeconds(3000)).build();
+		 service.start();		
+	}
+	
+	@BeforeClass
+	public void openingTheApplication() throws IOException {
+		driver.activateApp(pu.getTheDataFromPropertyfile("appPackage"));
+	}
+	
+	
+	@BeforeMethod
+	public void loggingIntoApp(){
+		
+		
+	}
 
 }
